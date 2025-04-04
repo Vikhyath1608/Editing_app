1b7372558c172f60ec82956e31f15fd6cf90e477
Vikhyath Rai MS <vikhyathraims0901@gmail.com>
2025-04-04 17:06:35 +0530

    merge-progress

--- preload.js
+++ preload.js
@@ -1,5 +1,6 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("electron", {
-    mergeVideos: (videoData) => ipcRenderer.invoke("merge-videos", videoData)
+    mergeVideos: (videoData) => ipcRenderer.invoke("merge-videos", videoData),
+    onMergeProgress: (callback) => ipcRenderer.on("merge-progress", (event, progress) => callback(progress))
 });
